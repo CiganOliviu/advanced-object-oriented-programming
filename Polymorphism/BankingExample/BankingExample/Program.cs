b2ed37d9dc73c6b4d90fbe468af85464909bad5c
Cigan Oliviu David <43005157+CiganOliviu@users.noreply.github.com>
2021-04-20 23:49:13 +0300

    Setup BankingApp Skeleton

---
+++ Polymorphism/BankingExample/BankingExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BankingExample
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+               
+        }
+    }
+}

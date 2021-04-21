adb662662322069cc124dc3327f61b2878d441e4
Cigan Oliviu David <43005157+CiganOliviu@users.noreply.github.com>
2021-04-21 15:30:23 +0300

    Update Banking Example for Polymorphism

--- Polymorphism/BankingExample/BankingExample/Program.cs
+++ Polymorphism/BankingExample/BankingExample/Program.cs
@@ -6,7 +6,16 @@ namespace BankingExample
     {
         private static void Main(string[] args)
         {
-               
+            Account savingsAccount = new Savings(5000, 0.8);
+            savingsAccount.SetDeposit(3000);
+            savingsAccount.PrintBalance();
+
+            Account checkingAccount = new CheckingAccount(5000);
+            checkingAccount.SetDeposit(1000);
+            checkingAccount.PrintBalance();
+
+            checkingAccount.SetWithdraw(3000);
+            checkingAccount.PrintBalance();
         }
     }
 }
